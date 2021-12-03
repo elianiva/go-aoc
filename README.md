6ab36d39fbbd3fb14fe3355e1ecd2f7b88787c8c
elianiva <dicha.arkana03@gmail.com>
2021-12-03 17:07:56 +0700

    skraaaa

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Advent Of Code with Go
 
-i don't even know if i'm going to finish this lol
+I don't even know if i'm going to finish this lol
+
+To run the solution, provide the `DAY` env variable.
+Example output:
+```bash
+go-aoc on  master (!?) via 🐹 v1.17.3
+❯ DAY=2 go run main.go
+======== DAY 2 ========
+PART 1: 1924923
+PART 2: 1982495697
+```
--- main.go
+++ main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"aoc/day1"
 	"aoc/day2"
+	"aoc/day3"
+	"fmt"
+	"os"
 )
 
+var days = map[string]func(){
+	"1": day1.Solve,
+	"2": day2.Solve,
+	"3": day3.Solve,
+}
+
 func main() {
-	day1.Solve()
-	day2.Solve()
+	day := os.Getenv("DAY")
+	if day == "" {
+		fmt.Println("DAY env variable wasn't provided.")
+		os.Exit(1)
+	}
+
+	days[day]()
 }
